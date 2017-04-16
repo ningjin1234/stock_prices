e5f86d9dea6757b724a82006ef5ca2a0a45e75f1
Ning Jin <ningjin@Nings-MacBook-Pro.local>
2017-04-16 17:59:13 -0400

    initial commit

---
+++ download_data.py
@@ -0,0 +1,30 @@
+import urllib.request as url
+import datetime as dt
+
+URL_TEMPLATE = 'http://ichart.finance.yahoo.com/table.csv?s=%s'
+
+def get_data_for_symbol(symbol, fname=None, path='.', mute=False):
+    try:
+        response = url.urlopen(URL_TEMPLATE % symbol)
+        lines = response.readlines()
+        if fname is None:
+            fname = '%s.txt' % symbol
+        fname = path + '/' + fname
+        with open(fname, 'wb') as fout:
+            for line in lines:
+                fout.write(line)
+        if not mute:
+            print('data collected for symbol %s [%s]' % (symbol, str(dt.datetime.now())))
+    except Exception as err:
+        print('failed to get data for symbol %s (%s)' % (symbol, str(err)))
+
+def get_data_for_symbols(symbols, path='.', mute=False):
+    for s in symbols:
+        get_data_for_symbol(s, path=path, mute=mute)
+
+symbols = []
+with open('symbols.txt', 'r') as fin:
+    for line in fin.readlines():
+        symbols += line.split(' ')
+symbols.remove('')
+get_data_for_symbols(symbols)
---
+++ prepare_data.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from dateutil.parser import parse as dparse
+import os
+
+def normalize_window_data(w):
+    res = []
+    for i in range(1, len(w)):
+        res.append((w[i]-w[i-1]) / w[i-1])
+    return res
+
+def window_to_str(w, debug_info=False):
+    res = ''
+    for i in range(len(w)):
+        if debug_info and i >= len(w)-2:
+            res += '\t' + str(w[i])
+            continue
+        if i > 0:
+            res += '\t'
+        res += '%.5g' % w[i]
+    return res
+
+def prepare_one_stock(fname, wsize=6, col_name='Adj Close', date_name='Date', first_date=None, last_date=None, debug_info=False):
+    df = pd.read_csv(fname)
+    df_r = df[::-1]
+    res = []
+    for i in range(0, df_r.shape[0]-wsize): # range may need to change after more target options become available
+        tdate = dparse(df_r.iloc[i+wsize][date_name])
+        if (first_date is not None and tdate < first_date) or (last_date is not None and tdate > last_date):
+            continue
+        window = list(df_r[i:(i+wsize)][col_name].values) # these are input variables
+        window.append(df_r.iloc[i+wsize][col_name]) # this is target, more options to come
+        window = normalize_window_data(window)
+        if debug_info:
+            window.append(fname[:-4])
+            window.append(df_r.iloc[i]['Date'])
+        res.append(window)
+    return res
+
+def prepare_all_in_dir(dname, outname, wsize=6, col_name='Adj Close', date_name='Date', first_date=None, last_date=None, debug_info=False):
+    fnames = os.listdir(dname)
+    with open(outname, 'w') as fout:
+        for f in fnames:
+            res = prepare_one_stock(dname+'/'+f, wsize=wsize, col_name=col_name, date_name=date_name, 
+                                    first_date=first_date, last_date=last_date, debug_info=debug_info)
+            for row in res:
+                fout.write(window_to_str(row, debug_info=debug_info)) 
+                fout.write('\n')
+
+prepare_all_in_dir('daily_prices', 'training.tsv', first_date=dparse('2010-01-01'), last_date=dparse('2015-12-31'), debug_info=False)
+
+prepare_all_in_dir('daily_prices', 'test.tsv', first_date=dparse('2016-01-01'), debug_info=False)
