a9b06f62929c7d5e76c04f3ce031bfe430df4d41
Ning Jin <ningjin@Nings-MacBook-Pro.local>
2017-04-17 18:38:26 -0400

    solve certificate verification failure

--- download_data.py
+++ download_data.py
@@ -1,5 +1,6 @@
 import urllib.request as url
 import datetime as dt
+import ssl
 
 '''
 potential source for more data:
@@ -15,13 +16,16 @@ https://www.quandl.com
 URL_TEMPLATE = 'http://ichart.finance.yahoo.com/table.csv?s=%s&g=%s'
 SDATE_TEMPLATE = '&a=%d&b=%d&c=%d'
 
+context = ssl._create_unverified_context()
+
 # start_date is a list of integers: [year, month, day]; month begins at 1 and gets adjusted inside the function
 def get_data_for_symbol(symbol, fname=None, path='.', mute=False, start_date=None, data_type='d'):
     try:
-        myurl = URL_TEMPLATE % (symbol, data_type)
+        url_symbol = '%5EGSPC' if symbol=='SP500' else symbol
+        myurl = URL_TEMPLATE % (url_symbol, data_type)
         if start_date is not None:
             myurl += SDATE_TEMPLATE % (start_date[1]-1, start_date[2], start_date[0])
-        response = url.urlopen(myurl)
+        response = url.urlopen(myurl, context=context)
         lines = response.readlines()
         if fname is None:
             fname = '%s.txt' % symbol
@@ -32,15 +36,19 @@ def get_data_for_symbol(symbol, fname=None, path='.', mute=False, start_date=Non
         if not mute:
             print('data collected for symbol %s [%s]' % (symbol, str(dt.datetime.now())))
     except Exception as err:
+        print(myurl)
         print('failed to get data for symbol %s (%s)' % (symbol, str(err)))
 
-def get_data_for_symbols(symbols, path='.', mute=False):
+def get_data_for_symbols(symbols, path='.', mute=False, start_date=None, data_type='d'):
+    print(symbols)
     for s in symbols:
-        get_data_for_symbol(s, path=path, mute=mute)
+        if s.strip() != '':
+            get_data_for_symbol(s, path=path, mute=mute, start_date=start_date, data_type=data_type)
 
 symbols = []
-with open('symbols.txt', 'r') as fin:
+with open('test_symbols.txt', 'r') as fin:
     for line in fin.readlines():
         symbols += line.split(' ')
-symbols.remove('')
-get_data_for_symbols(symbols)
+if '' in symbols:
+    symbols.remove('')
+get_data_for_symbols(symbols, start_date=[2010,1,1])
